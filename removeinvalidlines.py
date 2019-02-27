3693cae140e6d1c0de828182c4dc9394a36fbc1b
Peiyao Li <462046122@qq.com>
2019-02-28 00:25:22 +0800

    添加了去除无效行的removeinvalidlines.py文件

---
+++ removeinvalidlines.py
@@ -0,0 +1,14 @@
+import csv
+s = input("please input the file name:\n")
+ss = input("please input the output file name\n")
+totalNumber = 0
+with open(s, 'r') as file:
+    with open(ss, 'w', newline = '') as outfile:
+        lines = csv.reader(file)
+        writer = csv.writer(outfile)
+        for line in lines:
+            if(line[2]):
+                totalNumber += 1
+                writer.writerow(line)
+
+print('total lines:', totalNumber)
