d615869dd92d3a37f7ab9ce6d47f28f7583a801b
greatFredChen <38094428+greatFredChen@users.noreply.github.com>
2019-03-03 23:00:35 +0800

    更新脚本，改进了一些功能
    更新脚本，改进了一些功能

--- dataHelper.py
+++ dataHelper.py
@@ -2,6 +2,7 @@
 #使用说明:输入路径并且输入修改模式:D(默认)   C(多写了分号的情况)
 import csv
 filepath=input('请输入文件路径:')
+replaceStr=input('请输入想要替换成英文分号的字符:')
 filename=input('请输入修改后的文件名:')
 csv_file=csv.reader(open(filepath,'r'))
 out=open(filename+'.csv','w',newline='')
@@ -10,9 +11,12 @@ tot=0
 #对字符串进行处理
 def standardize(char):
     #可能需要替换中文分号
-    char=char.replace('；',' ')
+    if '；' in char:
+        char=char.replace('；',' ')
     #多写了英文分号的情况
     char=char.replace(';',' ')
+    #替换字符
+    char=char.replace(replaceStr,' ')
     #先把前后空格消去
     char.strip()
     #处理字符之间的空格
@@ -21,9 +25,10 @@ for content in csv_file:
     if content[2]!='':
         for i in range(2,5):
             content[i]=standardize(content[i])
+        tot=tot+1
+        content[0]=tot
         print(content)
         csv_write.writerow(content)
-        tot=tot+1
 print(tot)
 
 
